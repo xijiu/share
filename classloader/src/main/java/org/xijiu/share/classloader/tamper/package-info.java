f5f12710558b926402a84c80da2bdd83594e06ce
likangning <kangning.lkn@alibaba-inc.com>
2020-07-21 20:49:30 +0800

    init

---
+++ classloader/src/main/java/org/xijiu/share/classloader/tamper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 篡改类加载器，
+ */
+package org.xijiu.share.classloader.tamper;
---
+++ classloader/src/test/java/org/xijiu/share/AppTest.java
@@ -0,0 +1,18 @@
+package org.xijiu.share;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    }
+}
